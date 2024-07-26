9c3e62d61cea618229c63ab02e341f1e36ec1f43
Josh Kyte <jkyte@cloud-team.com>
2024-07-26 17:34:52 +0000

    Added deref function

--- cccutils.go
+++ cccutils.go
@@ -4,3 +4,12 @@ package ccc
 func Ptr[T any](t T) *T {
 	return &t
 }
+
+func Deref[T any](value *T) T {
+	var defaultValue T
+	if value != nil {
+		defaultValue = *value
+	}
+
+	return defaultValue
+}
