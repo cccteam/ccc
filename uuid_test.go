1660c8141b9ce5680b2231b91ab92f68c72f2901
Joseph Watson <joseph@cloud-team.com>
2024-08-01 22:15:28 +0000

    feature: Add Must() function for use in tests only

--- uuid_test.go
+++ uuid_test.go
@@ -7,6 +7,15 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
+// Must is a helper function to avoid the need to check for errors.
+func Must[T any](value T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+
+	return value
+}
+
 func TestNewUUID(t *testing.T) {
 	t.Parallel()
 
