566f81eb09d1941fb529110d89fd6e5fcfb2fa78
Joseph Watson <joseph@cloud-team.com>
2024-10-10 21:06:32 +0000

    Remove unneeded ResolvePermissions() and associated code

--- resourcestore/resourcestore.go
+++ resourcestore/resourcestore.go
@@ -2,7 +2,6 @@
 package resourcestore
 
 import (
-	"context"
 	"maps"
 	"slices"
 	"sync"
@@ -17,17 +16,13 @@ type (
 )
 
 type Store struct {
-	mu sync.RWMutex
-
-	enforcer accesstypes.Enforcer
-
+	mu            sync.RWMutex
 	tagStore      map[accesstypes.PermissionScope]tagStore
 	resourceStore map[accesstypes.PermissionScope]resourceStore
 }
 
-func New(e accesstypes.Enforcer) *Store {
+func New() *Store {
 	store := &Store{
-		enforcer:      e,
 		tagStore:      make(map[accesstypes.PermissionScope]tagStore, 2),
 		resourceStore: make(map[accesstypes.PermissionScope]resourceStore, 2),
 	}
@@ -80,26 +75,6 @@ func (s *Store) AddResource(scope accesstypes.PermissionScope, permission access
 	return nil
 }
 
-func (s *Store) ResolvePermissions(ctx context.Context, user accesstypes.User, domains ...accesstypes.Domain) (*accesstypes.ResolvedPermissions, error) {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-
-	resolvedTagPermissions, err := resolveTags(ctx, domains, s, user)
-	if err != nil {
-		return nil, err
-	}
-
-	resolvedResourcePermissions, err := resolveResource(ctx, domains, s, user)
-	if err != nil {
-		return nil, err
-	}
-
-	return &accesstypes.ResolvedPermissions{
-		Resources: resolvedResourcePermissions,
-		Tags:      resolvedTagPermissions,
-	}, nil
-}
-
 func (s *Store) permissions() []accesstypes.Permission {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
@@ -141,81 +116,6 @@ func (s *Store) resources() []accesstypes.Resource {
 	slices.Sort(resources)
 
 	return slices.Compact(resources)
-}
-
-func resolveTags(ctx context.Context, domains []accesstypes.Domain, s *Store, user accesstypes.User) (accesstypes.ResolvedTagPermissions, error) {
-	resolvedTagPermissions := make(accesstypes.ResolvedTagPermissions, len(domains))
-	for _, domain := range domains {
-		tagStore := s.tagStore[accesstypes.DomainPermissionScope]
-		if domain == accesstypes.GlobalDomain {
-			tagStore = s.tagStore[accesstypes.GlobalPermissionScope]
-		}
-		permissionResources := map[accesstypes.Permission][]accesstypes.Resource{}
-		for res, tags := range tagStore {
-			for tag, permissions := range tags {
-				for _, permission := range permissions {
-					permissionResources[permission] = append(permissionResources[permission], res.ResourceWithTag(tag))
-				}
-			}
-		}
-
-		for permission, resources := range permissionResources {
-			_, missing, err := s.enforcer.RequireResources(ctx, user, domain, permission, resources...)
-			if err != nil {
-				return nil, errors.Wrap(err, "accesstypes.Enforcer.RequireResources()")
-			}
-
-			for _, missingResource := range missing {
-				res, tag := missingResource.ResourceAndTag()
-				if resolvedTagPermissions[domain][res][tag] == nil {
-					if resolvedTagPermissions[domain][res] == nil {
-						if resolvedTagPermissions[domain] == nil {
-							resolvedTagPermissions[domain] = make(map[accesstypes.Resource]map[accesstypes.Tag]map[accesstypes.Permission]bool)
-						}
-						resolvedTagPermissions[domain][res] = make(map[accesstypes.Tag]map[accesstypes.Permission]bool)
-					}
-					resolvedTagPermissions[domain][res][tag] = make(map[accesstypes.Permission]bool)
-				}
-				resolvedTagPermissions[domain][res][tag][permission] = false
-			}
-		}
-	}
-
-	return resolvedTagPermissions, nil
-}
-
-func resolveResource(ctx context.Context, domains []accesstypes.Domain, s *Store, user accesstypes.User) (accesstypes.ResolvedResourcePermissions, error) {
-	resolvedResourcePermissions := make(accesstypes.ResolvedResourcePermissions, 2)
-	for _, domain := range domains {
-		resourceStore := s.resourceStore[accesstypes.DomainPermissionScope]
-		if domain == accesstypes.GlobalDomain {
-			resourceStore = s.resourceStore[accesstypes.GlobalPermissionScope]
-		}
-		permissionResources := map[accesstypes.Permission][]accesstypes.Resource{}
-		for res, permissions := range resourceStore {
-			for _, permission := range permissions {
-				permissionResources[permission] = append(permissionResources[permission], res)
-			}
-		}
-		for permission, resources := range permissionResources {
-			_, missing, err := s.enforcer.RequireResources(ctx, user, domain, permission, resources...)
-			if err != nil {
-				return nil, errors.Wrap(err, "accesstypes.Enforcer.RequireResources()")
-			}
-
-			for _, res := range missing {
-				if resolvedResourcePermissions[domain][res] == nil {
-					if resolvedResourcePermissions[domain] == nil {
-						resolvedResourcePermissions[domain] = make(map[accesstypes.Resource]map[accesstypes.Permission]bool)
-					}
-					resolvedResourcePermissions[domain][res] = make(map[accesstypes.Permission]bool)
-				}
-				resolvedResourcePermissions[domain][res][permission] = false
-			}
-		}
-	}
-
-	return resolvedResourcePermissions, nil
 }
 
 func (s *Store) List() map[accesstypes.Permission][]accesstypes.Resource {
