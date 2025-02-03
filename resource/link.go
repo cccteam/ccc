85b57a56ab9df7e7a026803a4b3ebf63087a9145
jkyte-ccc <josh@cloud-team.com>
2025-02-03 17:40:43 +0000

    Fixed link overflow errors and added NullLink type

--- resource/link.go
+++ resource/link.go
@@ -35,7 +35,10 @@ func (l *Link) DecodeSpanner(val any) error {
 }
 
 func (l Link) MarshalJSON() ([]byte, error) {
-	b, err := json.Marshal(l)
+	type linkAlias Link
+
+	link := linkAlias(l)
+	b, err := json.Marshal(link)
 	if err != nil {
 		return nil, errors.Wrap(err, "json.Marshal()")
 	}
@@ -51,22 +54,87 @@ func (l *Link) UnmarshalJSON(data []byte) error {
 		return nil
 	}
 
-	var link *Link
-	if err := json.Unmarshal(data, link); err != nil {
+	type linkAlias Link
+	link := linkAlias{}
+
+	if err := json.Unmarshal(data, &link); err != nil {
 		return errors.Wrap(err, "json.Unmarshal()")
 	}
 
-	if link == nil {
-		return nil
-	}
-
-	l.ID = link.ID
-	l.Resource = link.Resource
-	l.DisplayName = link.DisplayName
+	*l = Link(link)
 
 	return nil
 }
 
 func (l Link) IsNull() bool {
 	return l.ID.IsNil()
+}
+
+type NullLink struct {
+	Link
+	Valid bool
+}
+
+func (nl NullLink) EncodeSpanner() (any, error) {
+	if !nl.Valid {
+		return nil, nil
+	}
+
+	return spanner.NullJSON{Valid: nl.Valid, Value: nl.Link}, nil
+}
+
+func (nl *NullLink) DecodeSpanner(val any) error {
+	var jsonVal string
+	switch t := val.(type) {
+	case string:
+		jsonVal = t
+	case *string:
+		if t == nil {
+			return nil
+		}
+		jsonVal = *t
+	case nil:
+		return nil
+	default:
+		return errors.Newf("failed to parse %+v (type %T) as NullLink", val, val)
+	}
+
+	if err := nl.UnmarshalJSON([]byte(jsonVal)); err != nil {
+		return errors.Wrap(err, "nl.UnmarshalJSON()")
+	}
+
+	return nil
+}
+
+func (nl NullLink) MarshalJSON() ([]byte, error) {
+	if !nl.Valid {
+		return []byte("null"), nil
+	}
+
+	b, err := nl.Link.MarshalJSON()
+	if err != nil {
+		return nil, errors.Wrap(err, "json.Marshal()")
+	}
+
+	return b, nil
+}
+
+func (nl *NullLink) UnmarshalJSON(data []byte) error {
+	if data == nil {
+		return nil
+	}
+	if string(data) == "null" {
+		nl.Valid = false
+		return nil
+	}
+
+	link := Link{}
+	if err := link.UnmarshalJSON(data); err != nil {
+		return errors.Wrap(err, "link.UnmarshalJSON()")
+	}
+
+	nl.Link = link
+	nl.Valid = true
+
+	return nil
 }
