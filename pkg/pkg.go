22b86b81d06afca662419b35be01790609493f69
Joseph Watson <joseph@cloud-team.com>
2025-02-18 19:23:31 +0000

    refactor

--- pkg/pkg.go
+++ pkg/pkg.go
@@ -10,49 +10,51 @@ import (
 	"github.com/go-playground/errors/v5"
 )
 
-type PackageInfo struct {
+type PkgInfo struct {
 	AbsolutePath string
 	PackageName  string
 }
 
-func Info() (*PackageInfo, error) {
+func Info() (*PkgInfo, error) {
 	cwd, err := os.Getwd()
 	if err != nil {
 		return nil, errors.Wrap(err, "os.Getwd()")
 	}
 
+	var f *os.File
 	for {
-		f, err := os.Open(filepath.Join(cwd, "go.mod"))
+		f, err = os.Open(filepath.Join(cwd, "go.mod"))
 		if err != nil && !os.IsNotExist(err) {
 			return nil, errors.Wrap(err, "os.Open()")
-		} else if err == nil {
-			return func() (*PackageInfo, error) {
-				defer f.Close()
+		} else if os.IsNotExist(err) {
+			if cwd == "/" {
+				return nil, errors.New("pkg.Info(): reached root and did not find go.mod")
+			}
 
-				buff := bufio.NewScanner(f)
-				for buff.Scan() {
-					line := buff.Text()
-					if strings.HasPrefix(line, "module") {
-						parts := strings.Fields(line)
-						if len(parts) < 2 {
-							return nil, errors.New("packageInfo(): failed to find module path in go.mod")
-						}
+			cwd = filepath.Dir(cwd)
 
-						return &PackageInfo{
-							AbsolutePath: cwd,
-							PackageName:  parts[1],
-						}, nil
-					}
-				}
-
-				return nil, errors.New("packageInfo(): failed to find module directive in go.mod")
-			}()
+			continue
 		}
 
-		if cwd == "/" {
-			return nil, errors.New("packageInfo(): reached root and did not find go.mod")
-		}
-
-		cwd = filepath.Dir(cwd)
+		break
 	}
+	defer f.Close()
+
+	buff := bufio.NewScanner(f)
+	for buff.Scan() {
+		line := buff.Text()
+		if strings.HasPrefix(line, "module") {
+			parts := strings.Fields(line)
+			if len(parts) < 2 {
+				return nil, errors.New("pkg.Info(): failed to find module path in go.mod")
+			}
+
+			return &PkgInfo{
+				AbsolutePath: cwd,
+				PackageName:  parts[1],
+			}, nil
+		}
+	}
+
+	return nil, errors.New("pkg.Info(): failed to find module directive in go.mod")
 }
