16560ae6079c053ca598c60b7f0c0938016bdf57
mfrancis-ccc <mark.francis@cloud-team.com>
2025-02-19 16:21:36 +0000

    feat: add ccclint utility

---
+++ lint/cmd/ccclint/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime/debug"
+
+	"github.com/go-playground/errors/v5"
+	"github.com/spf13/pflag"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Printf("Error: %+v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	var golangiCiLintVersion string
+	pflag.StringVarP(&golangiCiLintVersion, "golangci-lint-version", "g", "v1.64", "golangci-lint CLI version")
+
+	buildInfo, available := debug.ReadBuildInfo()
+	if !available {
+		return errors.New("build info not available")
+	}
+
+	var pluginVersion string
+	pflag.StringVarP(&pluginVersion, "plugin-version", "p", buildInfo.Main.Version, "Version of the ccc/lint plugin")
+
+	var verbose bool
+	pflag.BoolVarP(&verbose, "verbose", "v", false, "Verbose output")
+
+	var help bool
+	pflag.BoolVarP(&help, "help", "h", false, "Print usage")
+
+	var version bool
+	pflag.BoolVar(&version, "version", false, "Print version")
+
+	pflag.Parse()
+
+	if help {
+		pflag.Usage()
+		return nil
+	}
+
+	if version {
+		fmt.Printf("ccclint version: %s\n", buildInfo.Main.Version)
+		return nil
+	}
+
+	if verbose {
+		fmt.Printf("golangci-lint version: %s\n", golangiCiLintVersion)
+		fmt.Printf("plugin version: %s\n", pluginVersion)
+	}
+
+	c := &ccclint{
+		golangiCiLintVersion: golangiCiLintVersion,
+		pluginVersion:        pluginVersion,
+		verbose:              verbose,
+	}
+
+	return c.run()
+}
