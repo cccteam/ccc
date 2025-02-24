7070caeb38c474f167763456eb7a2f8279deff95
jrowland-ccc <jordan.rowland@cloud-team.com>
2025-02-24 21:59:48 +0000

    increase complexity of testdata

--- resource/generation/testdata/resources/res1.go
+++ resource/generation/testdata/resources/res1.go
@@ -1,6 +1,28 @@
 package resources
 
+import "github.com/cccteam/ccc"
+
 type AddressType struct {
 	ID          string `spanner:"Id"`
 	Description string `spanner:"description"`
 }
+
+func (a AddressType) method() {}
+
+func doer() error {
+	return nil
+}
+
+type Status struct {
+	ID          ccc.UUID `spanner:"Id"`
+	Description string   `spanner:"description"`
+}
+
+type enum int
+
+const (
+	e1 enum = iota
+	e2
+)
+
+type alias = struct{}
