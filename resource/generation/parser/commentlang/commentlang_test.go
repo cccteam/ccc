edac3bfb2db3a1fec17d0c13537d2ddd22bb3532
jrowland-ccc <jordan.rowland@cloud-team.com>
2025-04-24 16:24:46 +0000

    refactor: add KeywordArgument interface to replace []string in keywordArgument map.
    this segments multi-arg keywords when they are used multiple times. for example if you use the foreignkey keyword multiple times on a single field, the arguments would've been in one contiguous slice []string. using a slice of []KeywordArguments we know that both arguments for foreignkey are in KeywordArguments[n]. this also enables multi-arg validation checks.
    
    - splits keywords into their own file
    - collapses two test functions into a single function

--- resource/generation/parser/commentlang/commentlang_test.go
+++ resource/generation/parser/commentlang/commentlang_test.go
@@ -9,7 +9,7 @@ import (
 
 func Test_commentLang(t *testing.T) {
 	type args struct {
-		comment string
+		comment []string
 		mode    commentlang.ScanMode
 	}
 	tests := []struct {
@@ -21,86 +21,36 @@ func Test_commentLang(t *testing.T) {
 		{
 			name: "multiline",
 			args: args{
-				comment: `/* @uniqueindex (Id, Description)
+				comment: []string{`/* @uniqueindex (Id, Description)
 
 @substring (SUBSTR(@self-4))
-
-*/`,
+@substring (Id)
+@substring(Other)
+*/`},
 				mode: commentlang.ScanStruct,
 			},
 			want: map[commentlang.Keyword][]string{
 				commentlang.UniqueIndex: {"Id, Description"},
-				commentlang.Substring:   {"SUBSTR(@self-4)"},
-			},
-		},
-		{
-			name: "multiline with and without space before arguments",
-			args: args{
-				comment: `/* @uniqueindex (Id)
-@uniqueindex(Id2)
-@substring (Id)
-@substring(Other)
-*/`,
-				mode: commentlang.ScanStruct,
-			},
-			want: map[commentlang.Keyword][]string{
-				commentlang.Substring:   {"Id", "Other"},
-				commentlang.UniqueIndex: {"Id", "Id2"},
+				commentlang.Substring:   {"SUBSTR(@self-4)", "Id", "Other"},
 			},
 		},
 		{
 			name: "singular",
-			args: args{comment: `// @primarykey (Id, Description)`, mode: commentlang.ScanStruct},
+			args: args{comment: []string{`// @primarykey (Id, Description)`}, mode: commentlang.ScanStruct},
 			want: map[commentlang.Keyword][]string{
 				commentlang.PrimaryKey: {"Id, Description"},
 			},
 		},
 		{
 			name:    "singular missing args error",
-			args:    args{comment: `// @primarykey`, mode: commentlang.ScanStruct},
+			args:    args{comment: []string{`// @primarykey`}, mode: commentlang.ScanStruct},
 			wantErr: true,
 		},
 		{
 			name:    "typo returns an error",
-			args:    args{comment: `// @primarkyey (Id, Description)`, mode: commentlang.ScanStruct},
+			args:    args{comment: []string{`// @primarkyey (Id, Description)`}, mode: commentlang.ScanStruct},
 			wantErr: true,
 		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-
-			got, err := commentlang.Scan([]string{tt.args.comment}, tt.args.mode)
-			if err != nil && tt.wantErr {
-				return
-			}
-			if err != nil {
-				t.Errorf("%s: commentlang.Scan() error (wantErr=%v): %s", t.Name(), tt.wantErr, err.Error())
-			}
-			if tt.wantErr {
-				t.Errorf("%s: commentlang.Scan() did not error (wantErr=%v)", t.Name(), tt.wantErr)
-
-				return
-			}
-
-			if diff := cmp.Diff(tt.want, got); diff != "" {
-				t.Errorf("%s: commentlang.Result() mismatch (-want +got):\n%s", t.Name(), diff)
-			}
-		})
-	}
-}
-
-func Test_commentLangFieldErrors(t *testing.T) {
-	type args struct {
-		comment []string
-		mode    commentlang.ScanMode
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    map[commentlang.Keyword][]string
-		wantErr bool
-	}{
 		{
 			name:    "primarykey with args returns an error when using ScanField mode",
 			args:    args{comment: []string{`// @primarykey (Id, Description)`}, mode: commentlang.ScanField},
@@ -130,7 +80,7 @@ func Test_commentLangFieldErrors(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
-			got, err := commentlang.Scan(tt.args.comment, tt.args.mode)
+			argMap, err := commentlang.Scan(tt.args.comment, tt.args.mode)
 			if err != nil && tt.wantErr {
 				return
 			}
@@ -141,6 +91,17 @@ func Test_commentLangFieldErrors(t *testing.T) {
 				t.Errorf("%s: commentlang.Scan() did not error (wantErr=%v)", t.Name(), tt.wantErr)
 
 				return
+			}
+
+			got := make(map[commentlang.Keyword][]string)
+			for key, args := range argMap {
+				if _, ok := got[key]; !ok {
+					got[key] = make([]string, 0)
+				}
+
+				for _, arg := range args {
+					got[key] = append(got[key], arg.Arguments()...)
+				}
 			}
 
 			if diff := cmp.Diff(tt.want, got); diff != "" {
