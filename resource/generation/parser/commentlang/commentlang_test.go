d2c435d5b80d4cd6d1ffa5baa5c9149362489ad3
jrowland-ccc <jordan.rowland@cloud-team.com>
2025-05-20 14:27:20 +0000

    refactor: simplify scanner modes by using ScanStruct and ScanField wrapper funcs instead of an interface parameter

--- resource/generation/parser/commentlang/commentlang_test.go
+++ resource/generation/parser/commentlang/commentlang_test.go
@@ -7,10 +7,9 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func Test_commentLang(t *testing.T) {
+func Test_ScanStruct(t *testing.T) {
 	type args struct {
 		comment string
-		mode    commentlang.ScanMode
 	}
 	tests := []struct {
 		name    string
@@ -24,7 +23,6 @@ func Test_commentLang(t *testing.T) {
 				comment: `@uniqueindex (Id, Description)
 					@foreignkey (Type) (StatusTypes(Id))
 					@foreignkey (Status) (Statuses(Id))`,
-				mode: commentlang.ScanStruct,
 			},
 			want: map[commentlang.Keyword][]string{
 				commentlang.UniqueIndex: {"Id, Description"},
@@ -33,24 +31,73 @@ func Test_commentLang(t *testing.T) {
 		},
 		{
 			name: "singular",
-			args: args{comment: `@primarykey (Id, Description)`, mode: commentlang.ScanStruct},
+			args: args{comment: `@primarykey (Id, Description)`},
 			want: map[commentlang.Keyword][]string{
 				commentlang.PrimaryKey: {"Id, Description"},
 			},
 		},
 		{
 			name:    "singular missing args error",
-			args:    args{comment: "@primarykey", mode: commentlang.ScanStruct},
+			args:    args{comment: "@primarykey"},
 			wantErr: true,
 		},
 		{
 			name:    "typo returns an error",
-			args:    args{comment: "@primarkyey (Id, Description)", mode: commentlang.ScanStruct},
+			args:    args{comment: "@primarkyey (Id, Description)"},
 			wantErr: true,
 		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			argMap, err := commentlang.ScanStruct(tt.args.comment)
+			if err != nil && tt.wantErr {
+				return
+			}
+			if err != nil {
+				t.Errorf("%s: commentlang.Scan() error (wantErr=%v): %s", t.Name(), tt.wantErr, err.Error())
+			}
+			if tt.wantErr {
+				t.Errorf("%s: commentlang.Scan() did not error (wantErr=%v)", t.Name(), tt.wantErr)
+
+				return
+			}
+
+			got := make(map[commentlang.Keyword][]string)
+			for key, args := range argMap {
+				if _, ok := got[key]; !ok {
+					got[key] = make([]string, 0)
+				}
+
+				for _, arg := range args {
+					got[key] = append(got[key], arg.Arg1)
+					if arg.Arg2 != nil {
+						got[key] = append(got[key], *arg.Arg2)
+					}
+				}
+			}
+
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("%s: commentlang.Result() mismatch (-want +got):\n%s", t.Name(), diff)
+			}
+		})
+	}
+}
+
+func Test_ScanField(t *testing.T) {
+	type args struct {
+		comment string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    map[commentlang.Keyword][]string
+		wantErr bool
+	}{
 		{
 			name:    "primarykey with args returns an error when using ScanField mode",
-			args:    args{comment: "@primarykey (Id, Description)", mode: commentlang.ScanField},
+			args:    args{comment: "@primarykey (Id, Description)"},
 			wantErr: true,
 		},
 		{
@@ -60,13 +107,12 @@ func Test_commentLang(t *testing.T) {
 @check(@self != 0)
 @foreignkey (Foobar(Id))
 @primarykey`,
-				mode: commentlang.ScanField,
 			},
 			wantErr: true,
 		},
 		{
 			name: "primarykey without args does not error when using ScanField mode",
-			args: args{comment: "@primarykey", mode: commentlang.ScanField},
+			args: args{comment: "@primarykey"},
 			want: map[commentlang.Keyword][]string{
 				commentlang.PrimaryKey: {},
 			},
@@ -75,7 +121,6 @@ func Test_commentLang(t *testing.T) {
 			name: "multiple singleline field comments",
 			args: args{
 				comment: "@primarykey\n@substring (@self - 4)\n@check (@self = 'S')",
-				mode:    commentlang.ScanField,
 			},
 			want: map[commentlang.Keyword][]string{
 				commentlang.PrimaryKey: {},
@@ -88,7 +133,7 @@ func Test_commentLang(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
-			argMap, err := commentlang.Scan(tt.args.comment, tt.args.mode)
+			argMap, err := commentlang.ScanField(tt.args.comment)
 			if err != nil && tt.wantErr {
 				return
 			}
