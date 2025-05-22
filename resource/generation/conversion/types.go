0add3b801a0390c84775d98465643214f249dd17
jrowland-ccc <jordan.rowland@cloud-team.com>
2025-05-22 20:24:15 +0000

    delete conversion package

--- resource/generation/conversion/types.go
+++
@@ -1,9 +0,0 @@
-package conversion
-
-// Used to generate Go functions converting types between databases
-type (
-	IntTo[T any]    = int
-	StringTo[T any] = string
-	Hidden[T any]   = T
-	View[T any]     struct{}
-)
