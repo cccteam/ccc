1aff63ec149c227046d1ef4f2cd22dfd305bdfa1
rmanuel-ccc <roshan.manuel@cloud-team.com>
2025-08-01 17:16:32 +0000

    feat: support 'tristate' values

--- resource/generation/types.go
+++ resource/generation/types.go
@@ -407,6 +407,9 @@ func (f resourceField) TypescriptDataType() string {
 	if f.typescriptType == "civilDate" {
 		return "Date"
 	}
+	if f.typescriptType == "boolean" && f.IsNullable {
+		return "TriState"
+	}
 
 	return f.typescriptType
 }
@@ -417,7 +420,7 @@ func (f resourceField) TypescriptDisplayType() string {
 	}
 
 	if f.typescriptType == "boolean" && f.IsNullable {
-		return "enumerated"
+		return "TriState"
 	}
 
 	return f.typescriptType
