dc3ec50b7c60c881708fb6e798bc3e86262eb5cf
jrowland-ccc <jordan.rowland@cloud-team.com>
2025-09-16 14:52:56 +0000

    feature: validate nullability of struct fields against spanner information schema

--- resource/generation/extract.go
+++ resource/generation/extract.go
@@ -2,6 +2,9 @@ package generation
 
 import (
 	"fmt"
+	"slices"
+	"strings"
+	"unicode"
 
 	"github.com/cccteam/ccc/resource/generation/parser"
 	"github.com/cccteam/ccc/resource/generation/parser/genlang"
@@ -10,11 +13,18 @@ import (
 
 func (c *client) extractResources(structs []*parser.Struct) ([]resourceInfo, error) {
 	resources := make([]resourceInfo, 0, len(structs))
+	var resourceErrors []error
 	for _, pStruct := range structs {
 		resourceName := pStruct.Name()
 		table, err := c.lookupTable(resourceName)
 		if err != nil {
 			return nil, err
+		}
+
+		if err := validateNullability(pStruct, table); err != nil {
+			resourceErrors = append(resourceErrors, err)
+
+			continue
 		}
 
 		resource := resourceInfo{
@@ -104,6 +114,10 @@ func (c *client) extractResources(structs []*parser.Struct) ([]resourceInfo, err
 		resources = append(resources, resource)
 	}
 
+	if len(resourceErrors) > 0 {
+		return nil, errors.Join(resourceErrors...)
+	}
+
 	return resources, nil
 }
 
@@ -122,4 +136,92 @@ func (c *client) structsToRPCMethods(structs []*parser.Struct) []rpcMethodInfo {
 	}
 
 	return rpcMethods
+}
+
+func validateNullability(pStruct *parser.Struct, table *tableMetadata) error {
+	nullableFields, err := fieldNullability(pStruct)
+	if err != nil {
+		return err
+	}
+
+	var errRows []string
+	for columnName, columnMetadata := range table.Columns {
+		if nullableFields[columnName] != columnMetadata.IsNullable {
+			errRow := fmt.Sprintf("| %-32s | %13t | %15t |", columnName, nullableFields[columnName], columnMetadata.IsNullable)
+			errRows = append(errRows, errRow)
+		}
+	}
+
+	if len(errRows) > 0 {
+		msg := strings.Builder{}
+		msg.WriteString("| ------------------------------------------------------------------ |\n")
+		msg.WriteString(fmt.Sprintf("| %*s |\n", -66, fmt.Sprintf("%*s", (66+len(pStruct.Name()))/2, pStruct.Name()))) // string centering voodoo black magic
+		msg.WriteString("| ------------------------------------------------------------------ |\n")
+		msg.WriteString("|               Name               | Can Nil Field | Can Null Column |\n")
+		msg.WriteString("| -------------------------------- | ------------- | --------------- |\n")
+
+		for i := range errRows {
+			msg.WriteString(errRows[i])
+			msg.WriteString("\n")
+		}
+
+		return errors.Newf("found mismatching nullability between the struct fields and columns:\n%s", msg.String())
+	}
+
+	return nil
+}
+
+func fieldNullability(pStruct *parser.Struct) (map[string]bool, error) {
+	nullableFields := make(map[string]bool)
+	var missingTags []string
+	for _, field := range pStruct.Fields() {
+		spannerTag, ok := field.LookupTag("spanner")
+		if !ok {
+			missingTags = append(missingTags, field.Name())
+		}
+
+		if slices.Contains([]string{
+			"*string",
+			"*bool",
+			"*uint", "*uint8", "*uint16", "*uint32", "*uint64",
+			"*int", "*int8", "*int16", "*int32", "*int64",
+			"*float32", "*float64",
+			"*time.Time",
+			"*interface {}",
+			"ccc.NullUUID",
+			"sql.NullBool", "sql.NullByte", "sql.NullFloat64", "sql.NullInt16", "sql.NullInt32", "sql.NullInt64", "sql.NullString", "sql.NullTime",
+			"spanner.NullBool", "spanner.NullDate", "spanner.NullFloat32", "spanner.NullFloat64", "spanner.NullInt64", "spanner.NullJSON", "spanner.NullNumeric", "spanner.NullString", "spanner.NullTime",
+			"*civil.Date",
+		}, field.Type()) {
+			nullableFields[spannerTag] = true
+
+			continue
+		}
+
+		if field.IsPointer() {
+			nullableFields[spannerTag] = true
+
+			continue
+		}
+
+		if name := field.DerefUnqualifiedType(); strings.HasPrefix(name, "Null") && unicode.IsUpper(rune(name[4])) {
+			nullableFields[spannerTag] = true
+
+			continue
+		}
+	}
+
+	if len(missingTags) > 0 {
+		msg := strings.Builder{}
+		for i := range missingTags {
+			if i > 0 {
+				msg.WriteString(", ")
+			}
+			msg.WriteString(missingTags[i])
+		}
+
+		return nil, errors.Newf("struct %s fields missing spanner tags: [%s]", pStruct.Name(), msg.String())
+	}
+
+	return nullableFields, nil
 }
