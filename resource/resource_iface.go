e19871b52e648472bed9c1345c27936cb0b6ebd7
Joseph Watson <joseph@cloud-team.com>
2025-10-10 18:58:00 +0000

    Cleanup documenation

--- resource/resource_iface.go
+++ resource/resource_iface.go
@@ -17,18 +17,24 @@ type UserPermissions interface {
 	User() accesstypes.User
 }
 
-// SpannerCommitter is an interface implemented by *spanner.Client and is used by PatchSet to allow mocking of *spanner.Client
+// SpannerCommitter is an interface that abstracts the Spanner client's transaction functionality.
+// It is used by PatchSet.SpannerApply() to allow for mocking the Spanner client in tests.
+// It is satisfied by *spanner.Client.
 type SpannerCommitter interface {
 	ReadWriteTransaction(ctx context.Context, f func(context.Context, *spanner.ReadWriteTransaction) error) (commitTimestamp time.Time, err error)
 }
 
-// TxnBuffer is an interface implemented by *spanner.ReadWriteTransaction, and is used by PatchSet to allow mocking of *spanner.ReadWriteTransaction
+// TxnBuffer is an interface that abstracts a Spanner read-write transaction,
+// allowing mutations to be buffered and queries to be executed within the transaction.
+// It is satisfied by *spanner.ReadWriteTransaction.
 type TxnBuffer interface {
 	BufferWrite(ms []*spanner.Mutation) error
 	spxapi.Querier
 }
 
-// SpannerBuffer is an interface implemented by *PatchSet that buffers its mutations in the *spanner.ReadWriteTransaction provided by TxnBuffer
+// SpannerBuffer is an interface for types that can buffer their Spanner mutations
+// into a transaction via the SpannerBuffer method. This is used for batching
+// operations.
 type SpannerBuffer interface {
 	SpannerBuffer(ctx context.Context, txn TxnBuffer, eventSource ...string) error
 }
@@ -38,7 +44,8 @@ type TxnRunner interface {
 	Execute(ctx context.Context, txn TxnBuffer) error
 }
 
-// TxnFuncRunner will have its ExecuteFunc() method called inside the *spanner.ReadWriteTransaction provided by TxnBuffer
+// TxnFuncRunner provides a way to execute a function within a read-write transaction.
+// This is useful for batching operations that need to be committed together.
 type TxnFuncRunner interface {
 	ExecuteFunc(ctx context.Context, runnerFn func(ctx context.Context, txn TxnBuffer) error) error
 }
