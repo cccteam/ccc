6eca5884880fafe4fb17fbe1f286f83e21ae13b1
Joseph Watson <72507396+jwatson-ccc@users.noreply.github.com>
2025-10-29 15:53:16 -0600

    refactor: Replace the CommitBuffer with BatchList() iterator for batching commits (#498)
    * refactor: Replace the CommitBuffer with BatchList() iterator for batching commits
    
    * Add tests

--- resource/queryset.go
+++ resource/queryset.go
@@ -369,6 +369,67 @@ func (q *QuerySet[Resource]) List(ctx context.Context, r Reader[Resource]) iter.
 	}
 }
 
+// BatchList executes the query and returns an iterator for the results in batches.
+func (q *QuerySet[Resource]) BatchList(ctx context.Context, r Reader[Resource], size int) iter.Seq[iter.Seq2[*Resource, error]] {
+	return func(yield func(iter.Seq2[*Resource, error]) bool) {
+		if size <= 0 {
+			yield(func(yield func(*Resource, error) bool) {
+				yield(nil, fmt.Errorf("invalid batch size %d, expected a positive integer", size))
+			})
+
+			return
+		}
+
+		next, stop := iter.Pull2(q.List(ctx, r))
+		defer stop()
+
+		var done bool
+		for !done {
+			done = true
+
+			firstRecord, err, ok := next()
+			if !ok {
+				return
+			}
+			if !yield(func(yield func(*Resource, error) bool) {
+				if err != nil {
+					yield(nil, err)
+
+					return
+				}
+
+				if !yield(firstRecord, nil) {
+					return
+				}
+
+				count := 1
+				for {
+					if count >= size {
+						done = false
+
+						return
+					}
+					record, err, ok := next()
+					if !ok {
+						return
+					}
+					if err != nil {
+						yield(nil, err)
+
+						return
+					}
+					if !yield(record, nil) {
+						return
+					}
+					count++
+				}
+			}) {
+				return
+			}
+		}
+	}
+}
+
 // SetWhereClause sets the filter condition for the query using a QueryClause.
 func (q *QuerySet[Resource]) SetWhereClause(qc QueryClause) {
 	q.filterAst = qc.tree
