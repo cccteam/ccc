eb9a3184ffa5f63fbe07af9b4b46e7cef682ac7c
Joseph Watson <72507396+jwatson-ccc@users.noreply.github.com>
2025-11-11 12:33:54 -0700

    feature: Implement a StartTrace() function to resolve package and function name. (#519)
    * feature: Implement a StartTrace() function to resolve package and function name.
    
    * cleanup linter
    
    * add caching to reduce cpu and allocs
    
    * Linter cleanup
    
    * add protection against in-lining

---
+++ tracing.go
@@ -0,0 +1,80 @@
+package ccc
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"strings"
+	"sync"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
+)
+
+const callerStackDepth = 1 // Caller of StartTrace is at depth 1
+
+var traceCache = &sync.Map{}
+
+type traceInfo struct {
+	tracerName string
+	spanName   string
+}
+
+// StartTrace uses runtime reflection to determine the fully qualified
+// package path and the function/method name of the caller.
+//
+// The Tracer name is set to the fully qualified package path
+// (e.g., "github.com/cccteam/ccc").
+// The Span name is set to the short function name (e.g., "Struct.Method()").
+//
+//go:noinline
+func StartTrace(ctx context.Context) (context.Context, trace.Span) {
+	pc, _, _, ok := runtime.Caller(callerStackDepth)
+	if !ok {
+		return otel.Tracer("unknown").Start(ctx, "unknown-func")
+	}
+
+	if i, ok := traceCache.Load(pc); ok {
+		switch info := i.(type) {
+		case traceInfo:
+			return otel.Tracer(info.tracerName).Start(ctx, info.spanName)
+		default:
+			panic(fmt.Sprintf("unexpected type %T in traceCache for pc %v", i, pc))
+		}
+	}
+
+	fn := runtime.FuncForPC(pc)
+	if fn == nil {
+		return otel.Tracer("unknown").Start(ctx, "unknown-func")
+	}
+
+	var tracerName, spanName string
+	qualifiedName := fn.Name()
+	lastSlash := strings.LastIndex(qualifiedName, "/")
+	splitIndex := -1
+	if lastSlash == -1 {
+		splitIndex = strings.Index(qualifiedName, ".")
+	} else {
+		dotIndexInPart := strings.Index(qualifiedName[lastSlash:], ".")
+		if dotIndexInPart != -1 {
+			splitIndex = lastSlash + dotIndexInPart
+		}
+	}
+
+	if splitIndex != -1 {
+		tracerName = qualifiedName[:splitIndex]
+		spanName = qualifiedName[splitIndex+1:]
+	} else {
+		tracerName = qualifiedName
+		spanName = qualifiedName
+	}
+
+	if strings.HasPrefix(spanName, "(*") {
+		spanName = strings.Replace(spanName[2:], ")", "", 1)
+	}
+
+	spanName += "()"
+	traceCache.Store(pc, traceInfo{tracerName: tracerName, spanName: spanName})
+
+	return otel.Tracer(tracerName).Start(ctx, spanName)
+}
---
+++ tracing_test.go
@@ -0,0 +1,139 @@
+package ccc
+
+import (
+	"context"
+	"testing"
+
+	"go.opentelemetry.io/otel"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/sdk/trace/tracetest"
+	"go.opentelemetry.io/otel/trace"
+)
+
+func TestStartTrace(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name       string
+		callingFn  func(context.Context) (context.Context, trace.Span)
+		tracerName string
+		spanName   string
+	}{
+		{
+			name:       "simple private function",
+			callingFn:  wrapperFunction,
+			tracerName: "github.com/cccteam/ccc",
+			spanName:   "wrapperFunction()",
+		},
+		{
+			name:       "in closure",
+			callingFn:  func(ctx context.Context) (context.Context, trace.Span) { return StartTrace(ctx) },
+			tracerName: "github.com/cccteam/ccc",
+			spanName:   "TestStartTrace.func1()",
+		},
+		{
+			name:       "nested in closure",
+			callingFn:  func(ctx context.Context) (context.Context, trace.Span) { return wrapperFunction(ctx) },
+			tracerName: "github.com/cccteam/ccc",
+			spanName:   "wrapperFunction()",
+		},
+		{
+			name:       "simple public function",
+			callingFn:  WrapperFunction,
+			tracerName: "github.com/cccteam/ccc",
+			spanName:   "WrapperFunction()",
+		},
+		{
+			name:       "ptr method of private struct",
+			callingFn:  (&wrapperStruct{}).PtrMethod,
+			tracerName: "github.com/cccteam/ccc",
+			spanName:   "wrapperStruct.PtrMethod()",
+		},
+		{
+			name:       "method of private struct",
+			callingFn:  (wrapperStruct{}).Method,
+			tracerName: "github.com/cccteam/ccc",
+			spanName:   "wrapperStruct.Method()",
+		},
+		{
+			name:       "ptr method of public struct",
+			callingFn:  (&WrapperStruct{}).PtrMethod,
+			tracerName: "github.com/cccteam/ccc",
+			spanName:   "WrapperStruct.PtrMethod()",
+		},
+		{
+			name:       "method of public struct",
+			callingFn:  (WrapperStruct{}).Method,
+			tracerName: "github.com/cccteam/ccc",
+			spanName:   "WrapperStruct.Method()",
+		},
+		{
+			name:       "method of public struct with closure",
+			callingFn:  (WrapperStruct{}).MethodWithClosure,
+			tracerName: "github.com/cccteam/ccc",
+			spanName:   "WrapperStruct.MethodWithClosure.func1()",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			exporter := tracetest.NewInMemoryExporter()
+			tp := sdktrace.NewTracerProvider(sdktrace.WithSyncer(exporter))
+			otel.SetTracerProvider(tp)
+
+			ctx, span := tc.callingFn(context.Background())
+			span.End()
+
+			if err := tp.ForceFlush(ctx); err != nil {
+				t.Fatal(err)
+			}
+
+			spans := exporter.GetSpans()
+			if len(spans) != 1 {
+				t.Fatalf("expected 1 span, got %d", len(spans))
+			}
+
+			if spans[0].Name != tc.spanName {
+				t.Errorf("expected span name %q, got %q", tc.spanName, spans[0].Name)
+			}
+
+			if spans[0].InstrumentationScope.Name != tc.tracerName {
+				t.Errorf("expected tracer name %q, got %q", tc.tracerName, spans[0].InstrumentationScope.Name)
+			}
+		})
+	}
+}
+
+func wrapperFunction(ctx context.Context) (context.Context, trace.Span) {
+	return StartTrace(ctx)
+}
+
+func WrapperFunction(ctx context.Context) (context.Context, trace.Span) {
+	return StartTrace(ctx)
+}
+
+type wrapperStruct struct{}
+
+func (w *wrapperStruct) PtrMethod(ctx context.Context) (context.Context, trace.Span) {
+	return StartTrace(ctx)
+}
+
+func (w wrapperStruct) Method(ctx context.Context) (context.Context, trace.Span) {
+	return StartTrace(ctx)
+}
+
+type WrapperStruct struct{}
+
+func (w *WrapperStruct) PtrMethod(ctx context.Context) (context.Context, trace.Span) {
+	return StartTrace(ctx)
+}
+
+func (w WrapperStruct) Method(ctx context.Context) (context.Context, trace.Span) {
+	return StartTrace(ctx)
+}
+
+func (w WrapperStruct) MethodWithClosure(ctx context.Context) (context.Context, trace.Span) {
+	return func() (context.Context, trace.Span) {
+		return StartTrace(ctx)
+	}()
+}
