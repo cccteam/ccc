37346ba36e43b233711c1a31c9d4a3b4df0d4018
jrowland-ccc <jordan.rowland@cloud-team.com>
2025-11-13 16:09:49 +0000

    feature: generate Method() for rpc methods

--- resource/generation/rpc.go
+++ resource/generation/rpc.go
@@ -18,7 +18,51 @@ func (r *resourceGenerator) runRPCGeneration() error {
 		return err
 	}
 
+	begin := time.Now()
+
+	if err := forEachGo(r.rpcMethods, r.generateRPCMethod); err != nil {
+		return err
+	}
+
 	if err := r.generateRPCInterfaces(); err != nil {
+		return err
+	}
+
+	log.Printf("Finished RPC file generation in %s", time.Since(begin))
+
+	return nil
+}
+
+func (r *resourceGenerator) generateRPCMethod(rpc *rpcMethodInfo) error {
+	fileName := generatedGoFileName(strings.ToLower(caser.ToSnake(rpc.Name())))
+	destinationFilePath := filepath.Join(r.rpc.Dir(), fileName)
+
+	file, err := os.Create(destinationFilePath)
+	if err != nil {
+		return errors.Wrap(err, "os.Create()")
+	}
+	defer file.Close()
+
+	tmpl, err := template.New(fmt.Sprintf("rpcFileTemplate:%q", rpc.Name())).Funcs(r.templateFuncs()).Parse(rpcFileTemplate)
+	if err != nil {
+		return errors.Wrap(err, "template.New().Parse()")
+	}
+
+	buf := bytes.NewBuffer(nil)
+	if err := tmpl.Execute(buf, map[string]any{
+		"Source":    r.rpc.Dir(),
+		"Package":   r.rpc.Package(),
+		"RPCMethod": rpc,
+	}); err != nil {
+		return errors.Wrap(err, "tmpl.Execute()")
+	}
+
+	formattedBytes, err := r.GoFormatBytes(file.Name(), buf.Bytes())
+	if err != nil {
+		return err
+	}
+
+	if err := r.WriteBytesToFile(file, formattedBytes); err != nil {
 		return err
 	}
 
