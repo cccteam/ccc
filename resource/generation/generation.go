34288f0bb245b04f0c32cf37c05933f573f21d18
jrowland-ccc <jordan.rowland@cloud-team.com>
2025-11-13 16:08:54 +0000

    feature: add forEachGo helper func

---
+++ resource/generation/generation.go
@@ -0,0 +1,39 @@
+package generation
+
+import (
+	"reflect"
+	"runtime"
+	"sync"
+
+	"github.com/go-playground/errors/v5"
+)
+
+func forEachGo[T any](s []*T, fn func(*T) error) error {
+	var (
+		wg      sync.WaitGroup
+		errChan = make(chan error)
+	)
+	for _, e := range s {
+		wg.Go(func() {
+			if err := fn(e); err != nil {
+				errChan <- err
+			}
+		})
+	}
+
+	go func() {
+		wg.Wait()
+		close(errChan)
+	}()
+
+	var errs []error
+	for err := range errChan {
+		errs = append(errs, err)
+	}
+
+	if len(errs) != 0 {
+		return errors.Wrap(errors.Join(errs...), runtime.FuncForPC(reflect.ValueOf(fn).Pointer()).Name())
+	}
+
+	return nil
+}
