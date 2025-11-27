3e84c298dfb7f87fc22ca32dbb337d66cef736fc
Joseph Watson <joseph@cloud-team.com>
2025-11-27 03:32:24 +0000

    add unit tests

--- securehash/hash_test.go
+++ securehash/hash_test.go
@@ -1,6 +1,7 @@
 package securehash
 
 import (
+	"database/sql/driver"
 	"reflect"
 	"testing"
 
@@ -218,6 +219,129 @@ func TestHash_DecodeSpanner(t *testing.T) {
 
 			if diff := (cmp.Diff(tt.want.underlying, h.underlying, cmp.AllowUnexported(argon2Key{}, Argon2Options{}, bcryptHash{}, BcryptOptions{}))); diff != "" {
 				t.Errorf("DecodeSpanner() mismatch (-want +got): underlying object does not match\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestHash_Value(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name    string
+		h       *Hash
+		want    driver.Value
+		wantErr bool
+	}{
+		{
+			name: "Bcrypt",
+			h: &Hash{
+				underlying: &bcryptHash{
+					hash: []byte("$2a$15$sJmPZT22fY8WmU5IlKvlWO7W6io2lxylIyElzH9KmfA/Nr6v/Vc4q"),
+				},
+			},
+			want: "$2a$15$sJmPZT22fY8WmU5IlKvlWO7W6io2lxylIyElzH9KmfA/Nr6v/Vc4q",
+		},
+		{
+			name: "Argon2ID",
+			h: &Hash{
+				underlying: &argon2Key{
+					key:  []byte("my-key"),
+					salt: []byte("my-salt"),
+					Argon2Options: Argon2Options{
+						memory:      12,
+						times:       8,
+						parallelism: 4,
+						saltLen:     7,
+						keyLen:      6,
+					},
+				},
+			},
+			want: `1$12$8$4$bXktc2FsdA==.bXkta2V5`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := tt.h.Value()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Hash.Value() error = %v, wantErr %v", err, tt.wantErr)
+
+				return
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Hash.Value() = %s, want %s", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHash_Scan(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name    string
+		hash    any
+		want    Hash
+		wantErr bool
+	}{
+		{
+			name: "Bcrypt string",
+			hash: "$2a$15$sJmPZT22fY8WmU5IlKvlWO7W6io2lxylIyElzH9KmfA/Nr6v/Vc4q",
+			want: Hash{
+				underlying: &bcryptHash{
+					hash: []byte("$2a$15$sJmPZT22fY8WmU5IlKvlWO7W6io2lxylIyElzH9KmfA/Nr6v/Vc4q"),
+					BcryptOptions: BcryptOptions{
+						cost: 15,
+					},
+				},
+			},
+		},
+		{
+			name: "Argon2ID string",
+			hash: `1$12$8$4$bXktc2FsdA==.bXkta2V5`,
+			want: Hash{
+				underlying: &argon2Key{
+					salt: []byte("my-salt"),
+					key:  []byte("my-key"),
+					Argon2Options: Argon2Options{
+						memory:      12,
+						times:       8,
+						parallelism: 4,
+						saltLen:     7,
+						keyLen:      6,
+					},
+				},
+			},
+		},
+		{
+			name: "nil",
+			hash: nil,
+			want: Hash{},
+		},
+		{
+			name:    "invalid type",
+			hash:    123,
+			want:    Hash{},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			h := &Hash{}
+			if err := h.Scan(tt.hash); (err != nil) != tt.wantErr {
+				t.Errorf("Hash.Scan() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if !tt.wantErr {
+				if diff := (cmp.Diff(tt.want.underlying, h.underlying, cmp.AllowUnexported(argon2Key{}, Argon2Options{}, bcryptHash{}, BcryptOptions{}))); diff != "" {
+					t.Errorf("Scan() mismatch (-want +got): underlying object does not match\n%s", diff)
+				}
 			}
 		})
 	}
