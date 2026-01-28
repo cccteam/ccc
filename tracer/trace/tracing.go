0b17691471ffa5146eb2849eea1494002de2a436
Joseph Watson <joseph@cloud-team.com>
2026-01-28 02:40:13 +0000

    feature: New tracer package for OpenTelemetry tracing

---
+++ tracer/trace/tracing.go
@@ -0,0 +1,96 @@
+// Package trace provides a convenient way to start OpenTelemetry traces
+// with automatically determined tracer and span names.
+//
+// The Start function inspects the call stack to find the calling function.
+// It uses the fully qualified package path of the caller as the tracer name
+// and the function name as the span name. This helps to reduce boilerplate
+// tracing code.
+//
+// To improve performance, the package caches the tracer and span names
+// after the first call from a specific location.
+package trace
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"strings"
+	"sync"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
+)
+
+const callerStackDepth = 1 // Caller of StartTrace is at depth 1
+
+var traceCache = &sync.Map{}
+
+type traceInfo struct {
+	tracerName string
+	spanName   string
+}
+
+// Span is the individual component of a trace. It represents a single named
+// and timed operation of a workflow that is traced. A Tracer is used to
+// create a Span and it is then up to the operation the Span represents to
+// properly end the Span when the operation itself ends.
+type Span = trace.Span
+
+// Start uses runtime reflection to determine the fully qualified
+// package path and the function/method name of the caller.
+//
+// The Tracer name is set to the fully qualified package path
+// (e.g., "github.com/cccteam/ccc").
+// The Span name is set to the short function name (e.g., "Struct.Method()").
+//
+//go:noinline
+func Start(ctx context.Context) (newCtx context.Context, span Span) {
+	pc, _, _, ok := runtime.Caller(callerStackDepth)
+	if !ok {
+		return otel.Tracer("unknown").Start(ctx, "unknown-func")
+	}
+
+	if i, ok := traceCache.Load(pc); ok {
+		switch info := i.(type) {
+		case traceInfo:
+			return otel.Tracer(info.tracerName).Start(ctx, info.spanName)
+		default:
+			panic(fmt.Sprintf("unexpected type %T in traceCache for pc %v", i, pc))
+		}
+	}
+
+	fn := runtime.FuncForPC(pc)
+	if fn == nil {
+		return otel.Tracer("unknown").Start(ctx, "unknown-func")
+	}
+
+	var tracerName, spanName string
+	qualifiedName := fn.Name()
+	lastSlash := strings.LastIndex(qualifiedName, "/")
+	splitIndex := -1
+	if lastSlash == -1 {
+		splitIndex = strings.Index(qualifiedName, ".")
+	} else {
+		dotIndexInPart := strings.Index(qualifiedName[lastSlash:], ".")
+		if dotIndexInPart != -1 {
+			splitIndex = lastSlash + dotIndexInPart
+		}
+	}
+
+	if splitIndex != -1 {
+		tracerName = qualifiedName[:splitIndex]
+		spanName = qualifiedName[splitIndex+1:]
+	} else {
+		tracerName = qualifiedName
+		spanName = qualifiedName
+	}
+
+	if strings.HasPrefix(spanName, "(*") {
+		spanName = strings.Replace(spanName[2:], ")", "", 1)
+	}
+
+	spanName += "()"
+	traceCache.Store(pc, traceInfo{tracerName: tracerName, spanName: spanName})
+
+	return otel.Tracer(tracerName).Start(ctx, spanName)
+}
---
+++ tracer/tracer.go
@@ -0,0 +1,85 @@
+// Package tracer provides convenience functions for setting up OpenTelemetry tracing
+// with Google Cloud Trace. It simplifies the creation of HTTP middleware for tracing
+// requests and configuring the global tracer provider.
+package tracer
+
+import (
+	"net/http"
+	"path"
+
+	texporter "github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/trace"
+	"github.com/GoogleCloudPlatform/opentelemetry-operations-go/propagator"
+	"github.com/go-playground/errors/v5"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
+)
+
+// NewGoogleCloudHandler creates a new HTTP middleware for OpenTelemetry tracing,
+// specifically configured for Google Cloud Trace.
+//
+// It uses the CloudTraceFormatPropagator and sets the span name to the request URL path.
+// The returned function can be used to wrap an http.Handler to add tracing.
+// Additional otelhttp.Option arguments can be passed to customize the behavior.
+func NewGoogleCloudHandler(o ...otelhttp.Option) func(http.Handler) http.Handler {
+	opts := []otelhttp.Option{
+		otelhttp.WithPropagators(propagator.CloudTraceFormatPropagator{}),
+		otelhttp.WithMessageEvents(otelhttp.ReadEvents, otelhttp.WriteEvents),
+		otelhttp.WithSpanNameFormatter(func(_ string, r *http.Request) string {
+			return r.URL.Path
+		}),
+	}
+
+	opts = append(opts, o...)
+
+	return func(next http.Handler) http.Handler {
+		return otelhttp.NewHandler(next, "", opts...)
+	}
+}
+
+// NewGoogleCloudTraceProvider creates and configures a new OpenTelemetry TracerProvider
+// for use with Google Cloud Trace. It sets up an exporter to send traces to the
+// specified Google Cloud project.
+//
+// The created TracerProvider is also set as the global tracer provider for the application.
+func NewGoogleCloudTraceProvider(loggingProjectID, serviceName string, sampler sdktrace.Sampler) (*sdktrace.TracerProvider, error) {
+	exporter, err := texporter.New(texporter.WithProjectID(loggingProjectID))
+	if err != nil {
+		return nil, errors.Wrap(err, "texporter.New()")
+	}
+
+	res, err := traceResource(serviceName)
+	if err != nil {
+		return nil, err
+	}
+
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(res),
+		sdktrace.WithSampler(sampler),
+	)
+	otel.SetTracerProvider(tp)
+
+	return tp, nil
+}
+
+func traceResource(serviceName string) (*resource.Resource, error) {
+	res, err := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName(serviceName),
+		),
+	)
+	if err != nil {
+		if resource.Default().SchemaURL() != semconv.SchemaURL {
+			return nil, errors.Newf("conflicting package versions installed: upgrade semconv package to go.opentelemetry.io/otel/semconv/v%s", path.Base(resource.Default().SchemaURL()))
+		}
+
+		return nil, errors.Wrap(err, "resource.Merge()")
+	}
+
+	return res, nil
+}
